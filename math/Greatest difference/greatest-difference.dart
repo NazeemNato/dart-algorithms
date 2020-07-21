a3b84b472abb9a22b8554dab888b35f86caf1eda
Muhammed Nazeem <39720973+NazeemNato@users.noreply.github.com>
2020-07-21 20:17:39 +0530

    ## My social media �

---
+++ math/Greatest difference/greatest-difference.dart
@@ -0,0 +1,21 @@
+void main(){
+  print(greatestDifference([2, 4, 6, 8, 16]));
+
+}
+greatestDifference(List numbers){
+  if(numbers.length == 0) return;
+  int largest = numbers[0];
+  int length = numbers.length;
+  int smallest = numbers[0];
+  for(int i = 0; i < length; i++){
+    int number = numbers[i];
+    
+    if (number > largest) {
+      largest = number;
+    }
+    if (number < smallest) {
+      smallest = number;
+    }
+  }
+  return largest - smallest;
+}
---
+++ math/Greatest difference/readme.md
@@ -0,0 +1,11 @@
+# Greatest Difference
+
+## My social media 🤪
+
+1. [Gmail](mailto:n4ze3m@gmail.com)
+
+
+2. [Twitter](https://twitter.com/juventusRuling)
+
+
+3. [Linkedin](https://www.linkedin.com/in/muhammad-nazeem-5ab092180/)
